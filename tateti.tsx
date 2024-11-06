86d2c01945399c6c7ae19a4e9f94cd253223936e
Augusto Riquelme <146036948+OTSUGUA14@users.noreply.github.com>
2024-11-06 17:59:33 -0300

    tateti react

---
+++ tateti.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react'
+import { ITateti } from '../types/ITateti'
+
+const Tateti:FC<ITateti> = ({valor}) => {
+  return (
+    <button></button>
+  )
+}
+
+export default Tateti
